efc07b78e67515e42868ceaae828a03055564928
Nicholas Williams <newilliams2@wsgc.com>
2020-09-07 18:55:35 -0700

    bug fix: fixed ungraceful shutdown

--- bridge_mock/mock.py
+++ bridge_mock/mock.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 @app.route('/api/<string:key>/lights/<int:n>/state', methods=['PUT'])
 def light_state(key, n):
     print(key, n, flask.request.data)
-    return ("PUT received, chief!", 200)
+    return ("PUT received, chief!", 201)
 
 if __name__ == "__main__":
     app.debug = True
--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-docker rm -f -v $(docker ps -a -q)
+docker rm -f -v $(docker ps -a -q) 2>/dev/null
+# whether or not succeeded, still want to exit gracefully
+exit 0
