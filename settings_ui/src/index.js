7e2219503f1709934295baee1fa561a86b2d2751
Nicholas <nwilliams770@gmail.com>
2020-09-30 18:24:34 -0700

    working: add settings ui

---
+++ settings_ui/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import style from './stylesheets/application.scss';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+
+// We might also see something like:
+  // document.addEventListener("DOMContentLoaded", () => {
+  // 	const root = document.getElementById("root");
+  // 	ReactDOM.render(<Root />, root);
+  // });
+// That isn't needed because our script is placed below our entry el
+// so it will always be loaded before our index.js is initiated
+
+ReactDOM.render(
+  <Root/>,
+  document.getElementById('root')
+);
---
+++ settings_ui/webpack.config.babel.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+  watch: true
+};
